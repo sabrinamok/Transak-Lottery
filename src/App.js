474e4969ac6c0087839e8bf07db30d3151e069fb
Adrien <adrien-barbier@hotmail.fr>
2022-04-25 14:04:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import { useEffect, useState } from "react";
+import lottery from "./lottery";
 
 function App() {
+  const [manager, setManager] = useState("");
+
+  useEffect(() => {
+    initManager();
+  }, []);
+
+  const initManager = async () => {
+    const manager = await lottery.methods.manager().call();
+    setManager(manager);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Lottery Contract</h2>
+      <p>This contract is managed by {manager}</p>
     </div>
   );
 }
